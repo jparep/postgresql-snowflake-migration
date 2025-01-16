ad3db8ee1eb6733968bf62e8b7e58d1fc8b71fbf
jparep <parepson@gmail.com>
2025-01-16 08:59:23 -0700

    Inset iunital data method implemented

--- psql/insert_data.py
+++ psql/insert_data.py
@@ -11,3 +11,10 @@ INITIAL_DATA = [
     (f"Employee {i}", 20 + i % 10, "Department A" if i % 2 == 0 else "Department B")
     for i in range(1, 21)
 ]
+
+def inisert_inital_data():
+    """Insert initial data into employees tabel."""
+    print("Inserting initial data...")
+    for data in INITIAL_DATA:
+        execute_query(data)
+    print("Initial data inserted successfully.")
