e6ef2630cc131ee2dcda234f408528fdc2193f86
jparep <parepson@gmail.com>
2025-01-16 08:01:41 -0700

    Implemented method to execite queries

--- psql/database.py
+++ psql/database.py
@@ -14,3 +14,17 @@ def get_connection():
     except psycopg2.error as e:
         print(f"Error connecting to the database: {e}")
         raise
+
+def excute_query(query, data=None):
+    """Excecute a query with optionsal parameters."""
+    connection = get_connection()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query, data)
+        connection.commit()
+    except psycopg2.Error as e:
+        print(f"Error executing query: {e}")
+        connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
