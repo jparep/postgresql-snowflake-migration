0ea154113b10a3fd7de7b5be6559c80ae0be3c65
jparep <parepson@gmail.com>
2025-01-16 07:15:15 -0700

    Config file created

---
+++ psql/config.py
@@ -0,0 +1,11 @@
+"""
+This fole contains the congiuration details for connecting to  the PostgresSQL databse
+"""
+
+DB_CONFIG = {
+    'DBNAME': 'test_db',
+    'USER': 'postgres',
+    'PASSWODR': 'admin',
+    'HOST': 'localhost',
+    'PORT': 5432
+}
