14ac136245e557b113a1474e1843daf33d1ab642
jparep <parepson@gmail.com>
2025-01-16 09:18:53 -0700

    Main file created

---
+++ psql/main.py
@@ -0,0 +1,11 @@
+from create_tables import create_table
+from insert_data import inisert_inital_data
+def main():
+    """Run the full database setup."""
+    print("Starting database setup...")
+    create_table()
+    inisert_inital_data()
+    print("Database setup completed.")
+    
+if __name__ = "__main__":
+    main()
