8860496263efb3556fcf165b774a2e600e5bdb75
jparep <parepson@gmail.com>
2025-01-16 07:54:59 -0700

    Fix psycopg2 import error

--- psql/database.py
+++ psql/database.py
@@ -2,14 +2,15 @@
 Crate resuable functions for databse operations.
 """
 
-import pysycopg2
+import psycopg2
+
 from config import DB_CONFIG
 
 def get_connection():
     """Establish and return connection to PostgrSQL database."""
     try:
-        connection = pysycopg2.connect(**DB_CONFIG)
+        connection = psycopg2.connect(**DB_CONFIG)
         return connection
-    except pysycopg2.error as e:
-        print("Error connecting to the database:" e)
+    except psycopg2.error as e:
+        print(f"Error connecting to the database: {e}")
         raise
