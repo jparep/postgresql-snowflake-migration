28cac16b71bc2e4c4050979e835841c9e0843453
jparep <parepson@gmail.com>
2025-01-16 21:04:23 -0700

    Created env

--- psql/config.py
+++ psql/config.py
@@ -1,11 +1,14 @@
 import os
+from dotenv import load_dotenv
 
 """This file contains the configuration details for connecting to the PostgresSQL database."""
+
+load_dotenv()
 
 DB_CONFIG = {
     'DBNAME': os.getenv('DB_NAME'),
     'USER': os.getenv('USER'),
     'PASSWORD': os.getenv('PASSWORD'),
     'HOST': os.getenv('HOST'),
-    'PORT': os.getenv('PORT')
+    'PORT': int(os.getenv('PORT'))
 }
