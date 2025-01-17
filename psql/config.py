e1017c2b957655ea0ad87d2d1fe7f4670301c5d0
jparep <parepson@gmail.com>
2025-01-16 21:13:16 -0700

    Updated config

--- psql/config.py
+++ psql/config.py
@@ -1,14 +1,19 @@
 import os
 from dotenv import load_dotenv
 
-"""This file contains the configuration details for connecting to the PostgresSQL database."""
+"""
+This file contains the configuration details for connecting to the PostgreSQL database.
+The credentials are loaded from environment variables using the dotenv package.
+"""
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# Database configuration dictionary
 DB_CONFIG = {
-    'DBNAME': os.getenv('DB_NAME'),
-    'USER': os.getenv('USER'),
-    'PASSWORD': os.getenv('PASSWORD'),
-    'HOST': os.getenv('HOST'),
-    'PORT': int(os.getenv('PORT'))
+    'dbname': os.getenv('DB_NAME', 'default_db'),  # Default value: 'default_db'
+    'user': os.getenv('DB_USER', 'postgres'),  # Default value: 'postgres'
+    'password': os.getenv('DB_PASSWORD', 'admin'),  # Default value: 'admin'
+    'host': os.getenv('DB_HOST', 'localhost'),  # Default value: 'localhost'
+    'port': int(os.getenv('DB_PORT', 5432))  # Default value: 5432
 }
