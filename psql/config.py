1d29c812312056e5e02030bd627f5669f068f804
jparep <parepson@gmail.com>
2025-01-16 20:57:58 -0700

    Use getenv for privacy

--- psql/config.py
+++ psql/config.py
@@ -1,11 +1,11 @@
-"""
-This file contains the configuration details for connecting to the PostgreSQL database.
-"""
+import os
+
+"""This file contains the configuration details for connecting to the PostgresSQL database."""
 
 DB_CONFIG = {
-    'DBNAME': 'test_db',  # Name of the database
-    'USER': 'postgres123',  # PostgreSQL username
-    'PASSWORD': 'admin',  # PostgreSQL password (consider storing securely)
-    'HOST': 'localhost',  # Host address
-    'PORT': 5432  # Port number
+    'DBNAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('USER'),
+    'PASSWORD': os.getenv('PASSWORD'),
+    'HOST': os.getenv('HOST'),
+    'PORT': os.getenv('PORT')
 }
