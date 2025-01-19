6ecb1619afc48411eeb7136f7090b5e12a190e0e
jparep <parepson@gmail.com>
2025-01-19 01:32:14 -0700

    Postgress connection

--- migration_script.py
+++ migration_script.py
@@ -4,3 +4,15 @@ import psycopg2
 import snowflake.connector
 import pandas as pd
 
+# Load environment variables from .env file
+load_dotenv()
+
+# PostgreSQL connectionb using environment variables
+conn_pg = psycopg2.connect(
+    host=os.getenv("POSTGRESS_HOST"),
+    port=os.getenv("PORTGRESS_PORT"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRESS_PASSWORD"),
+    database=os.getenv("POSTGRES_DATABASE")
+)
+
